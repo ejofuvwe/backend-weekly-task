ad0cc99222764f69f479c3c92dedd3892c66b944
UruAnn <uru.annabel@gmail.com>
2021-06-30 09:34:57 +0100

    week 4

---
+++ assignment/array.js
@@ -0,0 +1,6 @@
+let egg ='eggs'
+users = ['john', 'ejay', 'peter', 'dan']
+
+
+eggs.push(eggs)
+console.log(egg.push(eggs))
--- assignment/fizz.js
+++ assignment/fizz.js
@@ -1,15 +1,14 @@
 for(let i = 1; i <= 100; i++){
  
     
-    if(i%3==0 );{
+    if(i%3 === 0 && i%5!==0 );{
         console.log('Fizz')
     } 
-    if (i%5==0); {
+    if (i%5===0 && i%3!==0); {
         console.log('Buzz');
     }
-    if(i%3==0 && i%5 == 0 )
+    if(i%3===0 && i%5=== 0 )
     {console.log('FizzBuzz')};
 
-     {console.log(i);
 }
- }
+ 
